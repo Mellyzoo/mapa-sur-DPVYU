0e07499efd43629c12c8e88f449628ae4b2716a4
lean <mellyleo@hotmail.com>
2024-10-14 23:42:46 -0300

    muchos cambios de configuraciones, de rutas principalmente

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mapa-sur-DPVYU/",
+
   plugins: [
     react(),
     VitePWA({
+      base: "/mapa-sur-DPVYU",
+      manifest: false, // Desactiva la generación del manifiesto automático
       registerType: "autoUpdate",
       injectRegister: "auto", // Cambia esta configuración para asegurarte de que el SW se registre automáticamente en el lugar correcto
       includeAssets: [
@@ -15,51 +17,17 @@ export default defineConfig({
         "robots.txt",
         "apple-touch-icon.png",
       ],
-      manifest: {
-        name: "mapa-sur-DPVYU",
-        short_name: "mapa",
-        start_url: "/mapa-sur-DPVYU/",
-        scope: "/mapa-sur-DPVYU/",
-        lang: "es",
-        id: "mapa-sur-DPVYU",
-        description:
-          "mapa para marcar la geolocalizacion de edificios o viviendas para llegar más rapido",
-        theme_color: "#ffffff",
-        display: "standalone",
-        orientation: "portrait",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-      screenshots: [
-        {
-          src: "screenshot-desktop.png",
-          sizes: "1280x720",
-          type: "image/png",
-          form_factor: "wide",
-        },
-        {
-          src: "screenshot-mobile.png",
-          sizes: "720x1280",
-          type: "image/png",
-          form_factor: "narrow",
-        },
-      ],
+     
 
       workbox: {
         globDirectory: "dist",
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
         globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
-       skipWaiting: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        navigateFallback: "/index.html", // Ruta fallback en caso de que no se encuentre una ruta
+        navigateFallbackAllowlist: [/^\/mapa-sur-DPVYU\//], // Permitir la ruta "/mapaDPVyU/"
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/\w+\.tile\.openstreetmap\.org\/.*/i,
@@ -76,23 +44,20 @@ export default defineConfig({
               },
 
               fetchOptions: {
-                credentials: 'include', // Asegúrate de incluir credenciales si es necesario
+                credentials: "include", // Asegúrate de incluir credenciales si es necesario
               },
             },
           },
 
           // ... tus otras configuraciones de runtimeCaching ...
         ],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-        navigateFallback: "/mapa-sur-DPVYU/index.html", // Ruta fallback en caso de que no se encuentre una ruta
-        navigateFallbackAllowlist: [/^\/mapa-sur-DPVYU\//], // Permitir la ruta "/mapaDPVyU/"
+       
       },
 
       devOptions: {
         enabled: true,
         navigateFallback: "/mapa-sur-DPVYU/index.html",
-        suppressWarnings: true,
+        suppressWarnings: false,
         type: "module",
       },
     }),
@@ -100,8 +65,10 @@ export default defineConfig({
   build: {
     outDir: "dist", // Carpeta de salida para la build
     sourcemap: true, // Opcional, útil para debugging
+    publicDir: "public"
   },
   css: {
     devSourcemap: true,
   },
+  
 });
