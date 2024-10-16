5936b3cd6a39b298d596fed17c41137629c08d16
lean <mellyleo@hotmail.com>
2024-10-15 21:54:37 -0300

    LO ARREGLEEE

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,74 @@
-import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-
+  base: "/mapa-sur-DPVYU/",
   plugins: [
     react(),
     VitePWA({
-      base: "/mapa-sur-DPVYU/",
-      manifest: false, // Desactiva la generación del manifiesto automático
       registerType: "autoUpdate",
       injectRegister: "auto", // Cambia esta configuración para asegurarte de que el SW se registre automáticamente en el lugar correcto
+      manifest: {
+        name: "mapa-sur-DPVYU",
+        short_name: "mapa",
+        start_url: "/mapa-sur-DPVYU",
+        scope: "/",
+        id: "/",
+        lang: "es",
+        description:
+          "mapa para marcar la geolocalizacion de edificios o viviendas para llegar más rapido",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "./icons/pwa-64x64.png",
+            sizes: "64x64",
+            type: "any maskable",
+          },
+          {
+            src: "./icons/pwa-144x144.png",
+            sizes: "144x144",
+            purpose: "any",
+          },
+          {
+            src: "./icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "any maskable",
+          },
+          {
+            src: "./icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "any maskable",
+          },
+        ],
+        screenshots: [
+          {
+            src: "./screenshots/screenshot-desktop.png",
+            sizes: "1280x720",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "./screenshots/screenshot-mobile.png",
+            sizes: "720x1280",
+            type: "image/png",
+            form_factor: "narrow",
+          },
+        ],
+      },
+
       includeAssets: [
         "favicon.svg",
         "favicon.ico",
         "robots.txt",
         "apple-touch-icon.png",
       ],
-     
 
       workbox: {
+        sourcemap: true,
         globDirectory: "dist",
         globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
         globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
@@ -42,7 +91,6 @@ export default defineConfig({
               cacheableResponse: {
                 statuses: [0, 200],
               },
-
               fetchOptions: {
                 credentials: "include", // Asegúrate de incluir credenciales si es necesario
               },
@@ -51,24 +99,22 @@ export default defineConfig({
 
           // ... tus otras configuraciones de runtimeCaching ...
         ],
-       
       },
 
       devOptions: {
         enabled: true,
-        navigateFallback: "/mapa-sur-DPVYU/index.html",
-        suppressWarnings: false,
+        navigateFallback: "index.html",
         type: "module",
+        suppressWarnings: false,
       },
     }),
   ],
   build: {
     outDir: "dist", // Carpeta de salida para la build
     sourcemap: true, // Opcional, útil para debugging
-    publicDir: "public"
+    publicDir: "public",
   },
   css: {
     devSourcemap: true,
   },
-  
 });
